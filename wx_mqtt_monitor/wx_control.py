46c226c787647ee88b7cb97d1b9f5bac648e0f83
Anthony Leotta <tony@leotta.com>
2023-06-04 21:33:50 -0400

    adds a Multiple Broker Client Manager

--- wx_mqtt_monitor/wx_control.py
+++ wx_mqtt_monitor/wx_control.py
@@ -2,8 +2,82 @@ import wx
 import paho.mqtt.client as mqtt
 import json
 from datetime import datetime
+import time
+import threading
+
 
 DEVICE_PROPERTIES = ['state', 'brightness', 'color_temperature']
+
+
+class ClientManager:
+    def __init__(self, brokers, on_connect, on_message, retry_count=5):
+        self.clients = []
+        self.retry_count = retry_count
+
+        for broker in brokers:
+            client = mqtt.Client()
+            client.on_connect = on_connect
+            client.on_message = on_message
+            self.clients.append((client, broker))
+
+        self.thread = threading.Thread(target=self.run, daemon=True)
+        self.thread.start()
+
+    def run(self):
+        attempts = 0
+        while attempts < self.retry_count:
+            for client, broker in self.clients:
+                try:
+                    client.connect(broker, 1883, 60)
+                    client.loop_start()
+                    print(f"Connected to broker: {broker}")
+                    return
+                except Exception as e:
+                    print(f"Could not connect to broker: {broker}. Error: {str(e)}")
+                    time.sleep(1)  # pause before retrying
+            attempts += 1
+        print("Could not connect to any broker.")
+
+    def stop_all(self):
+        for client, broker in self.clients:
+            client.loop_stop(force=False)
+
+mqtt_connectios = {
+    "RPI4": {
+        "host": "192.168.1.22"
+    }, 
+    "SET": {
+        "host": "192.168.1.32"
+    }    
+}
+
+def connect_to_first_working_broker(client, brokers):
+    for broker in brokers:
+        try:
+            client.connect(broker, 1883, 60)
+            client.loop_start()
+            print(f"Connected to broker: {broker}")
+            return True
+        except Exception as e:
+            print(f"Could not connect to broker: {broker}. Error: {str(e)}")
+    return False
+
+def connect_to_brokers(client, brokers, retry_count):
+    attempts = 0
+    while attempts < retry_count:
+        for broker in brokers:
+            try:
+                client.connect(broker, 1883, 60)
+                client.loop_start()
+                print(f"Connected to broker: {broker}")
+                return True
+            except Exception as e:
+                print(f"Could not connect to broker: {broker}. Error: {str(e)}")
+                attempts += 1
+                if attempts >= retry_count:
+                    return False
+                time.sleep(1)  # pause before retrying
+    return False
 
 class PublishDialog(wx.Dialog):
     def __init__(self, parent, client):
@@ -94,7 +168,16 @@ class MainFrame(wx.Frame):
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
-        self.client.connect("192.168.1.32", 1883, 60)
+        #self.client.connect("192.168.1.32", 1883, 60)
+        #self.client.loop_start()
+
+        #brokers = ["192.168.1.22", "192.168.1.32"]
+        #if not connect_to_first_working_broker(self.client, brokers):
+        #    print("Could not connect to any broker.")
+        #    return
+        
+        self.client_manager = ClientManager(brokers, self.on_connect, self.on_message)
+        
 
         self.menu_bar = wx.MenuBar()
         self.file_menu = wx.Menu()
